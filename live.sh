ba6e93d160612b4aeb701a40bfd681c3eb784aab
Itz Random <100831398+ItzRandom23@users.noreply.github.com>
2024-10-02 14:49:24 +0530

    Update live.sh

--- live.sh
+++ live.sh
@@ -1,1 +1,6 @@
-ffmpeg -stream_loop -1 -re -i **VIDEO FILE NAME** -stream_loop -1 -re -i **AUDIO FILE MP3 LINK** -vcodec libx264 -pix_fmt yuvj420p -maxrate 20048k -preset veryfast -r 12 -framerate 30 -g 50 -c:a aac -b:a 128k -ar 44100 -strict experimental -video_track_timescale 1000 -b:v 150000k -f flv  rtmp://a.rtmp.youtube.com/live2/*Stream Key*
+ffmpeg -stream_loop -1 -re -i **VIDEO FILE NAME** -stream_loop -1 -re -i **AUDIO FILE MP3 LINK** \
+-vcodec libx264 -preset veryfast -profile:v high -pix_fmt yuv420p -r 60 -g 60 -keyint_min 60 \
+-b:v 10000k -maxrate 11000k -bufsize 15000k -c:a aac -b:a 128k -ar 48000 -ac 2 -movflags +faststart \
+-tune zerolatency -vf "scale=1920:1080" \
+-f flv rtmp://a.rtmp.youtube.com/live2/*Stream Key* \
+-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 2 -y
